a251685bc1cdb8d2ca21348f11a322e60a12d140
Andi <andi@akidee.de>
2010-07-25 03:09:01 +0200

    Now includes JSON stuff for clients that do not support it. Additionally Object.id() and Object reference support for full JSON interchange of many:1 and curcular references. Updated tests.

--- test.js
+++ test.js
@@ -6,6 +6,13 @@ require('extensions');
 
 
 
+// Object
+
+
+
+	// Object.extend()
+
+
 var p = {a: 1};
 var f = function () {};
 f.prototype = {b:1};
@@ -37,6 +44,9 @@ a(
 
 
 
+	// Object.extendDeep()
+
+
 p = {d: {e: 1}};
 q = {d: {e: 2, f: 3}};
 
@@ -63,7 +73,6 @@ a(
 );
 
 
-
 p = {};
 q = {d: {e: 2, f: 3}};
 
@@ -71,4 +80,253 @@ q = {d: {e: 2, f: 3}};
 d(
 	Object.extendDeep(p, q).d.f,
 	3
+);
+
+
+
+	// Object.fromPrototype()
+
+
+var proto = {
+	a:1,
+	b:2
+};
+
+var ext = {
+	b:3
+};
+
+var res = Object.fromPrototype(proto, ext);
+
+d(
+	res,
+	{b:3}
+);
+
+d(
+	res.__proto__,
+	{a:1, b:2}
+);
+
+
+
+	// Object.id()
+
+
+var p = {};
+
+Object.id(p);
+
+a(
+	p.$id,
+	1
+);
+
+var q = {};
+
+Object.id(q);
+
+d(
+	q,
+	{$id:2}
+);
+
+
+	
+	// Object.resetRefs()
+
+
+var o = {c:1};
+
+Object.resetRefs(o);
+
+d(
+	o.$refs,
+	[]
+);
+
+p = [];
+
+Object.resetRefs(p);
+
+d(
+	p[p.length - 1],
+	{$$refs:[]}
+);
+
+
+
+	// Object.getRefs()
+
+
+d(
+	Object.getRefs(o),
+	[]	
+);
+
+d(
+	Object.getRefs(p),
+	[]	
+);
+
+
+
+	// Object.addRef()
+
+
+d(
+	Object.addRef(o, ['a'], ['#']),
+	[['a'], ['#']]
+);
+
+d(
+	Object.addRef(p, ['a'], ['#']),
+	[['a'], ['#']]	
+);
+
+global.xy = true;
+
+Object.addRef(o, ['b'], ['xy']);
+Object.addRef(p, ['b'], ['xy']);
+
+
+
+	// Object.findRefs()
+
+
+var q = {};
+q.a = q;
+q.b = q;
+
+Object.findRefs(q);
+
+a(
+	q.$id,
+	3
+);
+
+d(
+	q.$refs,
+	[
+		['a'], ['#'],
+		['b'], ['#']
+	]
+);
+
+Object.findRefs(q);
+
+a(
+	q.$id,
+	3
+);
+
+d(
+	q.$refs,
+	[
+		['a'], ['#'],
+		['b'], ['#']
+	]
+);
+
+
+
+	// Object.resolveRefs()
+
+
+Object.resolveRefs(o);
+
+d(
+	o.a,
+	o
+);
+
+d(
+	o.b,
+	undefined
+);
+
+d(
+	Object.getRefs(o),
+	[['b'], ['xy']]
+);
+
+Object.resolveRefs(o, true);
+
+d(
+	o.a,
+	o
+);
+
+d(
+	o.b,
+	true
+);
+
+d(
+	Object.getRefs(o),
+	[]
+);
+
+
+
+// Array.prototype
+
+
+
+	// Array.prototype.clone()
+	
+
+var array = [1,2,3],
+	cl = array.clone();
+cl.push(4);
+a(
+	array[3],
+	undefined
+);
+
+
+
+// String.prototype
+
+
+	// String.prototype.escapeXml()
+	
+	
+var str = '<>\n<>""\'\'\n';
+a(
+	str.escapeXml(),
+	'&lt;&gt;\n&lt;&gt;&quot;&quot;&#039;&#039;\n'
+);
+a(
+	str.escapeXml(true),
+	'&lt;&gt;<br/>&lt;&gt;&quot;&quot;&#039;&#039;<br/>'
+);
+
+
+
+// Test extended JSON capabilities
+
+global.MyClass = function MyClass (data) {
+
+	Object.extend(this, data);
+}
+global.MyClass.clientUsesClass = true;
+global.MyClass.classPath = ['MyClass'];
+
+
+var myObject = new MyClass({a:1,b:2,c:[7,8,9]});
+myObject.d = myObject;
+
+var s = JSON.stringify(Object.findRefs(myObject), JSON.REPLACER_REFS_AND_CLASSES);
+
+var _myObject = Object.resolveRefs(JSON.parse(s, JSON.REVIVER_CLASSES));
+
+a(
+	_myObject instanceof MyClass,
+	true
+);
+
+a(
+	_myObject.d,
+	_myObject
 );
